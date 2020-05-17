c88adb72e4ab25ce857f78936d3aabff144fef37
Justin <jmlhunt@gmail.com>
2020-05-17 14:31:53 -0700

    Commit

---
+++ Files_All.sql
@@ -0,0 +1,17 @@
+select d.Filename,d.CurrentStatusID,vv.VariableID,vv.ValueText, vv.ConfigurationID, vv.RevisionNo
+from Documents d
+              inner join Revisions r on d.DocumentID = r.DocumentID and d.LatestRevisionNo = r.RevNr
+              inner join VariableValue vv on r.DocumentID = vv.DocumentID and d.LatestRevisionNo = r.RevNr
+
+              inner join
+                      (
+                          select  vv2.DocumentID, max(vv2.RevisionNo) as maxVar
+                          from VariableValue vv2
+                          group by vv2.DocumentID
+                      ) vv2
+                      on vv.DocumentID = vv2.DocumentID
+                      and vv.RevisionNo = vv2.maxVar
+
+              where d.DocumentID = 2441
+              and vv.VariableID = 46
+              and vv.ConfigurationID = 2
