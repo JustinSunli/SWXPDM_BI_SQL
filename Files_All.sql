ca79ed6af06421d8ac4e3de38f7564e318673af2
Justin <jmlhunt@gmail.com>
2020-05-17 22:32:55 -0700

    FIles_All met return requirements.

--- Files_All.sql
+++ Files_All.sql
@@ -1,17 +1,20 @@
-select *--d.Filename,d.CurrentStatusID,vv.VariableID,vv.ValueText, vv.ConfigurationID, vv.RevisionNo
+
+declare @max int
+set @max  =   (
+                select max(vv2.RevisionNo) as maxver
+                from VariableValue vv2
+                where vv2.ConfigurationID = 2
+                and vv2.DocumentID = 2441
+                and vv2.VariableID = 46
+                and vv2.ConfigurationID = 2
+                )
+
+select *
 from Documents d
-         inner join Revisions r on d.DocumentID = r.DocumentID and d.LatestRevisionNo = r.RevNr
-         inner join VariableValue vv on r.DocumentID = vv.DocumentID and d.LatestRevisionNo = r.RevNr
+inner join VariableValue vv on d.DocumentID = vv.DocumentID --and d.LatestRevisionNo = vv.RevisionNo
+where vv.RevisionNo = @max
+and vv.VariableID = 46
+and d.DocumentID = 2441
+and vv.ConfigurationID = 2
 
---               inner join
---                       (
---                           select  vv2.DocumentID, max(vv2.RevisionNo) as maxVar
---                           from VariableValue vv2
---                           group by vv2.DocumentID
---                       ) vv2
---   on vv.DocumentID = vv2.DocumentID
---                      and vv.RevisionNo = vv2.maxVar
 
-where d.DocumentID = 2441
-  and vv.VariableID = 46
-  and vv.ConfigurationID = 2
