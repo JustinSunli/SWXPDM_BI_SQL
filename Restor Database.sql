c1bf74169acf7f64c48d2cca6a9ce3bcd0644fd2
Justin <jmlhunt@gmail.com>
2020-05-22 23:50:27 -0700

    Add web address to Linux restore code

--- Restor Database.sql
+++ Restor Database.sql
@@ -1,5 +1,5 @@
 
-
+--https://docs.microsoft.com/en-us/sql/linux/sql-server-linux-migrate-restore-database?view=sql-server-ver15
 RESTORE DATABASE Blount_vault
 FROM DISK = '/home/justin/Downloads/2020_05_15_BlountVault.bak'
 WITH MOVE 'Blount_Vault' TO '/var/opt/mssql/data/Blount_Vault.mdf',
